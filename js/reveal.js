586b9f9f0b104c7b9b1993c707eca7e3a3fd5c4d
Oleksiy Dubovyk <dubovyk@gmail.com>
2021-04-14 22:50:37 +0300

    chore

--- js/reveal.js
+++ js/reveal.js
@@ -43,11 +43,11 @@
 			height: 700,
 
 			// Factor of the display size that should remain empty around the content
-			margin: 0.04,
+			margin: 0.1,
 
 			// Bounds for smallest/largest possible scale to apply to content
-			minScale: 0.2,
-			maxScale: 2.0,
+			minScale: 0.1,
+			maxScale: 4.0,
 
 			// Display presentation control arrows
 			controls: true,
